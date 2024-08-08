b89455c198de39693d788dd862966e1537b77625
jundev95 <junmop950dev@gmail.com>
2024-08-08 17:01:35 +0900

    함수화 + 채팅기능 구현중

--- src/features/chat/chat-room/actions/ChatRoomDetailsAction.js
+++ src/features/chat/chat-room/actions/ChatRoomDetailsAction.js
@@ -4,7 +4,8 @@ import api from '../../../../utils/connections/api';
 const chatRoomDetailsByChatRoomId = createAsyncThunk(
   'chatRoomDetails/chatRoomDetailsByChatRoomId',
   async ({ chatRoomId }) => {
-    const response = await api.get(`/chats/chat-rooms/${chatRoomId}`);
+    // const response = await api.get(`/chats/chat-rooms/${chatRoomId}`);
+    const response = await api.post(`/chats/chat-rooms/${chatRoomId}/join`);
     return response.data;
   }
 );
