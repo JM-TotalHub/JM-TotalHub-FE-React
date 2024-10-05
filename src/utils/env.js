b3b0519a2c6bf922b2bab89eb68f3982924d0953
jundev95 <junmop950dev@gmail.com>
2024-10-05 17:20:18 +0900

    ssl 적용을 위한 환경변수에 요청 프로토콜 변경

--- src/utils/env.js
+++ src/utils/env.js
@@ -6,13 +6,21 @@ const isProd = process.env.REACT_APP_REACT_SERVER_ENV_STATUS === 'prod';
 
 const ENV = {
   // Express 서버 api (배포: 엔진엑스 서버 / 개발: 로컬 시그널 서버)
+  // EXPRESS_SERVER_BASE_URL: isProd
+  //   ? `http://${process.env.REACT_APP_REACT_SERVER01_EC2_HOST}/api`
+  //   : `http://${process.env.REACT_APP_EXPRESS_LOCAL_HOST}:${process.env.REACT_APP_EXPRESS_LOCAL_POST}`,
+
+  // SIGNAL_SERVER_SOCKET_BASE_URL: isProd
+  //   ? `http://${process.env.REACT_APP_REACT_SERVER01_EC2_HOST}`
+  //   : `http://${process.env.REACT_APP_SIGNAL_LOCAL_HOST}:${process.env.REACT_APP_SIGNAL_LOCAL_POST}`,
+
   EXPRESS_SERVER_BASE_URL: isProd
-    ? `http://${process.env.REACT_APP_REACT_SERVER01_EC2_HOST}/api`
+    ? `https://${process.env.REACT_APP_REACT_SERVER01_EC2_HOST}/api` // HTTPS로 변경
     : `http://${process.env.REACT_APP_EXPRESS_LOCAL_HOST}:${process.env.REACT_APP_EXPRESS_LOCAL_POST}`,
 
   SIGNAL_SERVER_SOCKET_BASE_URL: isProd
-    ? `http://${process.env.REACT_APP_REACT_SERVER01_EC2_HOST}`
-    : `http://${process.env.REACT_APP_SIGNAL_LOCAL_HOST}:${process.env.REACT_APP_SIGNAL_LOCAL_POST}`,
+    ? `wss://${process.env.REACT_APP_REACT_SERVER01_EC2_HOST}` // WSS로 변경
+    : `ws://${process.env.REACT_APP_SIGNAL_LOCAL_HOST}:${process.env.REACT_APP_SIGNAL_LOCAL_POST}`,
 
   // 엔진엑스 서버
   REACT_APP_NGINX_SERVER01_EC2_HOST:
