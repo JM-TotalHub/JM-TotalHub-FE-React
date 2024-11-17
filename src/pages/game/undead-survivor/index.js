a2e696bc7008fb2d37167edf2f6311387663a139
jundev95 <junmop950dev@gmail.com>
2024-11-17 22:08:24 +0900

    이름 수정

--- src/pages/game/undead-survivor/index.js
+++ src/pages/game/undead-survivor/index.js
@@ -3,10 +3,11 @@ import { Unity, useUnityContext } from 'react-unity-webgl';
 
 const UndeadSurvivor = () => {
   const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: '/game/undead-survivor3/unity-build/test03.loader.js',
-    dataUrl: '/game/undead-survivor3/unity-build/test03.data',
-    frameworkUrl: '/game/undead-survivor3/unity-build/test03.framework.js',
-    codeUrl: '/game/undead-survivor3/unity-build/test03.wasm',
+    loaderUrl: '/game/undead-survivor3/unity-build/undead-survivor.loader.js',
+    dataUrl: '/game/undead-survivor3/unity-build/undead-survivor.data',
+    frameworkUrl:
+      '/game/undead-survivor3/unity-build/undead-survivor.framework.js',
+    codeUrl: '/game/undead-survivor3/unity-build/undead-survivor.wasm',
     // loaderUrl: '/game/undead-survivor/unity-build/undead-survivor.loader.js',
     // dataUrl: '/game/undead-survivor/unity-build/undead-survivor.data.br',
     // frameworkUrl:
